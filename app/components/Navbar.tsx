0b98579113f3917eb22e7869ebcc07c627bf3006
unknown <riquezlvs@gmail.com>
2024-05-18 12:21:08 -0300

    Aba Aluno

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { MdKeyboardArrowLeft } from "react-icons/md";
 
 export function Navbar({ lblCategoria, lblTituloCat } : { lblTituloCat: string, lblCategoria:string}){
@@ -5,12 +6,13 @@ export function Navbar({ lblCategoria, lblTituloCat } : { lblTituloCat: string, 
         <main className="m-auto w-full bg-grey-bg text-white flex flex-col ">
             <div className="p-12 max-w-[115rem]">
                 <p className="font-light mx-16">{`${lblCategoria} > ${lblTituloCat} `}</p>
-                <div className="flex flex-row text-center font-medium text-3xl gap-x-4 my-2 items-center">
-                    <MdKeyboardArrowLeft  className=" text-5xl text-center"/>
-                    <h1 className="text-center">{lblTituloCat}</h1>
-                </div>
+                <Link href="/">
+                    <div className="flex flex-row text-center font-medium text-3xl gap-x-4 my-2 items-center">
+                        <MdKeyboardArrowLeft  className=" text-5xl text-center"/>
+                        <h1 className="text-center">{lblTituloCat}</h1>
+                    </div>
+                </Link>
             </div>
-
         </main>
     )
 }
